253c0c684e72b1be7085faca64739bd592ceb5a5
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2024-01-26 00:55:28 +0800

    #!/bin/bash python server.py --loader Transformers --verbose --auto-devices --gpu-memory 22 22 --disk --trust-remote-code --api --api-port 5000

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python server.py --loader Transformers --verbose --auto-devices --gpu-memory 22500MiB 23500MiB --trust-remote-code --api --api-port 5000
+python server.py --loader Transformers --verbose --auto-devices --gpu-memory 22 22 --disk --trust-remote-code --api --api-port 5000
