1f370dacf8aa899b111c01bb2091b102b6902578
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2024-01-28 14:49:19 +0800

    #!/bin/bash python server.py --auto-devices --gpu-memory 22 22 --verbose --trust-remote-code --api --api-port 5000

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python server.py --auto-devices --gpu-memory 22 22 --use_flash_attention_2 --verbose --trust-remote-code --api --api-port 5000
+python server.py --auto-devices --gpu-memory 22 22 --verbose --trust-remote-code --api --api-port 5000
