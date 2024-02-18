a8f1fc0e530c03a3af102b8528f3873c7574dffc
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2024-02-18 21:48:50 +0800

    ADD MODEL: 01-ai_Yi-34B-Chat-4bits

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python server.py --auto-devices --gpu-memory 22 22 --verbose --trust-remote-code --api --api-port 5000
+python server.py --auto-devices --gpu-memory 23 23 --disk --verbose --trust-remote-code --api --api-port 5000
